9b960561dbb1c678cf9b5b7cab20f94bb12ca709
Alfie Wilkinson <alfie@whyougetproductions.com>
2024-08-10 19:44:18 +0100

    Update nav.tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,23 +1,20 @@
 export default function Nav() {
   return (
     <nav className="bg-gray-800 flex flex-col w-full mx-auto py-3 justify-center">
-      <ul className="flex gap-16 justify-center  text-gray-300 text-sm">
+      <ul className="flex gap-16 justify-center text-gray-300 text-sm">
         <li>
-          
-              <a href="/">Whyouget Productions</a
-           </a>
+          <a href="/">Whyouget Productions</a>
         </li>
-<a href="/">
-        <li>Home</li> 
-</a>
-<a href="https://docs.whyougetproductions.com">
-        <li>Docs</li> 
-</a>
-<a href="httpe://docs.whyougetproductions.com">
-        <li>Status</li> 
-</a>
-
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="https://docs.whyougetproductions.com">Docs</a>
+        </li>
+        <li>
+          <a href="https://docs.whyougetproductions.com">Status</a>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
